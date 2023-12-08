d26c97bca5f5e91cb8422e554871c212b54548fb
Ador-25 <abtahee.ador25@gmail.com>
2023-12-08 23:48:55 +0600

    night mode added

--- WebApplication1/tailwind.config.js
+++ WebApplication1/tailwind.config.js
@@ -8,18 +8,8 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: '#3490dc', // Example primary color
-                secondary: '#ffed4a', // Example secondary color
-            },
-            fontSize: {
-                '4xl': '2.5rem', // Example custom font size
-                '5xl': '3rem',
-            },
-            backgroundColor: {
-                'dark': '#1a202c', // Dark background color
-            },
-            textColor: {
-                'dark': '#ffffff', // Dark text color
+                night: '#1a202c', // Dark background color
+                nightText: '#ffffff', // Text color for night mode
             },
         },
     },
