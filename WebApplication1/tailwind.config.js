a009ba0711ddf35fb0678fb583069aa97dee6fbe
Ador-25 <abtahee.ador25@gmail.com>
2023-12-08 23:40:23 +0600

    extensions applied

--- WebApplication1/tailwind.config.js
+++ WebApplication1/tailwind.config.js
@@ -1,10 +1,27 @@
+// tailwind.config.js
 module.exports = {
     content: [
-        './Pages/**/*.cshtml',
-        './Views/**/*.cshtml'
+        './Views/**/*.cshtml',
+        './wwwroot/**/*.html',
+        './wwwroot/**/*.js',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#3490dc', // Example primary color
+                secondary: '#ffed4a', // Example secondary color
+            },
+            fontSize: {
+                '4xl': '2.5rem', // Example custom font size
+                '5xl': '3rem',
+            },
+            backgroundColor: {
+                'dark': '#1a202c', // Dark background color
+            },
+            textColor: {
+                'dark': '#ffffff', // Dark text color
+            },
+        },
     },
     plugins: [],
 }
